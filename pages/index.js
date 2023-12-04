3c5d8e9aad152b4a9b4797001bb4ee0bf0168230
alicevaliha <alicevaliha@gmail.com>
2023-12-04 09:39:34 +0300

    adding all webservices

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,31 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Press buttons to access data retrievement ! <code>pages/index.js</code>
+          Press buttons to access data retrievement ! 
         </p>
         <form action=" https://statistiques-foot.vercel.app/api/v1/joueurs" method="get">
           <button type="submit">Acceder aux données des joueurs</button>
+        </form>
+        <form action=" https://statistiques-foot.vercel.app/api/v1/equipes" method="get">
+          <button type="submit">Acceder aux données des équipes</button>
+        </form>
+        <form action=" https://statistiques-foot.vercel.app/api/v1/competitions" method="get">
+          <button type="submit">Acceder aux données des compétitioins</button>
+        </form>
+        <form action=" https://statistiques-foot.vercel.app/api/v1/matches" method="get">
+          <button type="submit">Acceder aux données des matchs</button>
+        </form>
+        <form action=" https://statistiques-foot.vercel.app/api/v1/passes" method="get">
+          <button type="submit">Acceder aux données des actions des passes</button>
+        </form>
+        <form action=" https://statistiques-foot.vercel.app/api/v1/possessions" method="get">
+          <button type="submit">Acceder aux données des actions des possessions</button>
+        </form>
+        <form action=" https://statistiques-foot.vercel.app/api/v1/actions" method="get">
+          <button type="submit">Acceder aux données des actions des actions</button>
+        </form>
+        <form action=" https://statistiques-foot.vercel.app/api/v1/cartons" method="get">
+          <button type="submit">Acceder aux données des actions des cartons</button>
         </form>
       </main>
 
